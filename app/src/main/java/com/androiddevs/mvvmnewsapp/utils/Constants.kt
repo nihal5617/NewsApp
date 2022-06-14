e0cde9616f4fe1b4b638a44a8b2cd9fe285db6e0
Nihal Vishal Gupta <nihalng786@gmail.com>
2022-06-14 14:42:27 +0530

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY="1429c314999546d58a4e13c5894a04f5"
+        const val BASE_URL="https://newsapi.org"
+    }
+}
