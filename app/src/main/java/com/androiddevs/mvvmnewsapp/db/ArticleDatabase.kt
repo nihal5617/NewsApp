3b8fa775a54206379e65af8bf5a55f9a166ecc7c
Nihal Vishal Gupta <nihalng786@gmail.com>
2022-06-16 20:58:57 +0530

    Updated

--- app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDatabase.kt
@@ -8,7 +8,7 @@ import com.androiddevs.mvvmnewsapp.models.Article
 
 @Database(
     entities = [Article::class],
-    version = 1
+    version = 2
 )
 @TypeConverters(Converters::class)
 abstract class ArticleDatabase :RoomDatabase(){
@@ -28,6 +28,8 @@ abstract class ArticleDatabase :RoomDatabase(){
                 context.applicationContext,
                 ArticleDatabase::class.java,
                 "article_db.db"
-            ).build()
+            )
+                .fallbackToDestructiveMigration()
+                .build()
     }
 }
